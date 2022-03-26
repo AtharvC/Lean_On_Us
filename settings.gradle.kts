d3702f9f521155cfb99c83fba562cf9d8c778456
AtharvC <atharvchakole370@gmail.com>
2022-03-26 14:39:39 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    
+}
+rootProject.name = "Lean_On_Us"
+
+
+include(":androidApp")
+include(":shared")
+
---
+++ shared/src/commonMain/kotlin/me/shared/Platform.kt
@@ -0,0 +1,5 @@
+package me.shared
+
+expect class Platform() {
+    val platform: String
+}
